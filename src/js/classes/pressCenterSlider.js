1a7da22ad7357c6759bcfc90640d0a7ccff373a6
Артем Сафин <artemsafin.webdev@gmail.com>
2020-01-08 13:01:22 +0300

    Дописаны скрипты пресс-центра

--- src/js/classes/pressCenterSlider.js
+++ src/js/classes/pressCenterSlider.js
@@ -4,10 +4,13 @@ class PressCenterSlider {
     constructor(element) {
         this.elements = {
             tabItems: Array.from(element.querySelectorAll('.press-center-slider__tab-item')),
-            tabButtons: Array.from(element.querySelectorAll('.press-center-slider__tab-btn')).concat(Array.from(element.querySelectorAll('.press-center-slider__tab-more-menu-link')))
+            tabButtons: Array.from(element.querySelectorAll('.press-center-slider__tab-btn')).concat(Array.from(element.querySelectorAll('.press-center-slider__tab-more-menu-link'))),
+            prevButton: element.querySelector('.press-center-slider__controls-btn--prev'),
+            nextButton: element.querySelector('.press-center-slider__controls-btn--next')
         };
 
         this.state = {
+            tabActiveIndex: 0,
             sliders: [],
             sliderOptions: {
                 watchOverflow: true,
@@ -16,20 +19,78 @@ class PressCenterSlider {
             }
         };
 
-
-        console.dir(this.elements.tabButtons);
-
         this.initializeSliders();
+        this.setActiveTab(this.state.tabActiveIndex);
+        this.setTabButtonsListeners();
+        this.setSliderButtonListeners();
     }
 
-    initializeTabs() {}
+    setActiveTab(index) {
+        const { tabButtons, tabItems } = this.elements;
+        tabItems.forEach(item => item.classList.remove('active'));
+        tabButtons.forEach(button => button.classList.remove('active'));
+        this.state.tabActiveIndex = index;
+        tabItems[index].classList.add('active');
+        tabButtons[index].classList.add('active');
+        this.checkButtonsActivity();
+    }
+
+    checkButtonsActivity() {
+        const { prevButton, nextButton } = this.elements;
+        const { sliders } = this.state;
+        const currentSlider = sliders[this.state.tabActiveIndex];
+        prevButton.disabled = false;
+        nextButton.disabled = false;
+        console.log(currentSlider.progress);
+        if (currentSlider.progress === 0 || currentSlider.isBeginning) {
+            prevButton.disabled = true;
+        }
+        if (currentSlider.progress === 1 || currentSlider.isEnd) {
+            nextButton.disabled = true;
+        }
+    }
+
+    handleSliderControlsClick(event, direction, btn) {
+        event.preventDefault();
+        const { sliders } = this.state;
+        const currentSlider = sliders[this.state.tabActiveIndex];
+
+        if (direction === 'prev' && btn.disabled === false) {
+            currentSlider.slidePrev();
+        } else if (direction === 'next' && btn.disabled === false) {
+            currentSlider.slideNext();
+        } else {
+            console.log('No button action available');
+        }
+    }
+
+    setSliderButtonListeners() {
+        const { prevButton, nextButton } = this.elements;
+        prevButton.addEventListener('click', this.handleSliderControlsClick.bind(this, event, 'prev', prevButton));
+        nextButton.addEventListener('click', this.handleSliderControlsClick.bind(this, event, 'next', nextButton));
+    }
+
+    setTabButtonsListeners() {
+        const { tabButtons } = this.elements;
+        tabButtons.forEach((btn, index) => {
+            btn.addEventListener('click', event => {
+                event.preventDefault();
+                this.setActiveTab(index);
+            });
+        });
+    }
 
     initializeSliders() {
         const { tabItems } = this.elements;
         tabItems.forEach(item => {
             const swiperContainer = item.querySelector('.swiper-container');
+            const swiperInstance = new Swiper(swiperContainer, this.state.sliderOptions);
 
-            this.state.sliders.push(new Swiper(swiperContainer, this.state.sliderOptions));
+            swiperInstance.on('progress', () => {
+                this.checkButtonsActivity();
+            });
+
+            this.state.sliders.push(swiperInstance);
         });
     }
 }
