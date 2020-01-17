7abfc395d245438f7a9609edac2242e44b75077c
Артем Сафин <artemsafin.webdev@gmail.com>
2020-01-17 17:35:16 +0300

    Изменены функции аккоредонов

--- src/js/functions/accordeons.js
+++ src/js/functions/accordeons.js
@@ -1,23 +1,26 @@
 function openAccordeon(element) {
     element.style.maxHeight = 'none';
-    element.style.overflow = 'visible';
     const computedStyle = getComputedStyle(element);
     const computedHeight = computedStyle.height;
-
-    const offsetHeight = element.offsetHeight;
     element.style.maxHeight = '';
-    element.style.overflow = '';
-    
-    console.log('Offset height', offsetHeight);
-
     setTimeout(() => {
+        const transitionEndHandler = () => {
+            console.log('Tranisitionnd Initiated');
+            element.style.maxHeight = 'none';
+            element.removeEventListener('transitionend', transitionEndHandler);
+        };
+        element.addEventListener('transitionend', transitionEndHandler);
         element.style.maxHeight = `${computedHeight}`;
     }, 20);
 }
 
 function closeAccordeon(element) {
-    element.style.maxHeight = '';
-    element.style.overflow = '';
+    const computedStyle = getComputedStyle(element);
+    const computedHeight = computedStyle.height;
+    element.style.maxHeight = `${computedHeight}`;
+    setTimeout(() => {
+        element.style.maxHeight = '';
+    }, 20);
 }
 
 export { openAccordeon, closeAccordeon };
